3405d202dc541a410c792e7e448390a992b9c5c0
Olha Abrosimova <abrosimova2020@ukr.net>
2022-09-12 11:42:39 +0300

    add search form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
 	return (
-		<div className="App">
-			<header className="App-header">Dictionary app</header>{" "}
-			<button className="btn btn-primary">search</button>
+		<div className="App container">
+			<header className="App-header">Dictionary app</header> <Dictionary />
 		</div>
 	);
 }
-
-
