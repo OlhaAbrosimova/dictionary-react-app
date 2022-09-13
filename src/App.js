c3cbd2b458dd5c4eac463fe2097eb1f87071871d
Olha Abrosimova <abrosimova2020@ukr.net>
2022-09-13 10:56:50 +0300

    added git hub link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ export default function App() {
 	return (
 		<div className="App container">
 			<header className="App-header">Dictionary app</header> <Dictionary />
+			<footer>
+				This app was coded by{" "}
+				<a
+					href="https://olhaabrosimova.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Olha Abrosimova
+				</a>{" "}
+				and it is{" "}
+				<a
+					href="https://github.com/OlhaAbrosimova/dictionary-react-app"
+					target="_blank"
+					rel="noreferrer"
+				>
+					open-sourced
+				</a>
+			</footer>
 		</div>
 	);
 }
